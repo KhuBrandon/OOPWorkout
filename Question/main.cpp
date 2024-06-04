bd763df593a88b3ca828a320d306d5a935bf1d77
KimBrandon <163233156+KhuBrandon@users.noreply.github.com>
2024-06-04 20:31:53 +0900

    Create main.cpp

---
+++ Question/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "fruit.h"
+
+using namespace std;
+int main() {
+	fruit apple, pear, peach;
+	cout << " " << &apple << endl;
+	cout << " " << &pear << endl;
+	cout << " " << &peach << endl;
+}
